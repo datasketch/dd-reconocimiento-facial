287e1643ce050b756e77d3831822baf5251c0b34
YosmanProgramer <Yosman.higuita028@gmail.com>
2021-08-23 15:51:31 -0500

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: {
+    enabled: process.env.HUGO_ENVIRONMENT === 'production',
+    content: ['./layouts/**/*.html'],
+    options: {
+      safelist: []
+    }
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
