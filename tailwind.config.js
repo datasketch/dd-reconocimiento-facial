0417926accf86864b3574fef7de08548a848d52e
YosmanProgramer <Yosman.higuita028@gmail.com>
2021-08-24 15:28:04 -0500

    work on home section desktop version completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,24 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "merriweather-sans": ['Merriweather Sans', 'sans-serif'],
+        "raleway": ['Raleway', 'sans-serif']
+      },
+      backgroundImage: {
+        'banner-home-lg': 'url(/images/home/banner-home-lg.png)',
+        'banner-explore-data-home-lg': 'url(/images/home/banner-explore-data-lg.png)',
+        'methodology-lg': 'url(/images/home/methodology.png)',
+        'footer-lg': 'url(/images/public/footer.png)'
+      },
+      colors: {
+        white: '#FBF9F3',
+        'lemon-green': '#00E5BA',
+        'blue-dark': '#29366D',
+        'blue-title': '#006999',
+      }
+    },
   },
   variants: {
     extend: {},
