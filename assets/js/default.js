ca585e053fa3f9d641aa1b71a2e452f63d934ba3
YosmanProgramer <Yosman.higuita028@gmail.com>
2021-08-31 09:07:21 -0500

    add controls in the menu mobile

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,7 +1,21 @@
 const nav = document.querySelector(".nav");
 const btnHamburguer = document.querySelector(".menu-icon");
+const overlay = document.querySelector(".overlay");
 
-btnHamburguer.addEventListener("click", function () {
+const menu = () => {
   btnHamburguer.classList.toggle("menu-icon--active");
   nav.classList.toggle("nav--active");
+  overlay.classList.toggle("hidde");
+};
+
+btnHamburguer.addEventListener("click", menu);
+
+overlay.addEventListener("click", menu);
+
+window.addEventListener("scroll", function (e) {
+  if (e.isTrusted && !overlay.classList.contains("hidde")) menu();
+});
+
+window.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !overlay.classList.contains("hidde")) menu();
 });
