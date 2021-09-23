e3f30955d7363ab5513d8269b778b0e4fbfe9cd5
YosmanProgramer <Yosman.higuita028@gmail.com>
2021-09-23 09:03:20 -0500

    add filter and message in the provider section

--- assets/js/api/proveedores-identificados.js
+++ assets/js/api/proveedores-identificados.js
@@ -4,6 +4,12 @@ import { Modal } from "../utils/modal";
 const providerParent = document.querySelector(".provider-container");
 const res = document.querySelector("#data-proveedoresIdentificados");
 const body = document.querySelector("body");
+const buttonParent = document.querySelector("#button-container");
+const buttonFilter = document.querySelectorAll(".button-filter");
+const providerMessage = document.querySelectorAll(".provider-message");
+const filterProvider = document.querySelectorAll(".filter-provider");
+const selectCountries = document.querySelector("#filterByCountries");
+const selectSector = document.querySelector("#filterBySector");
 res.remove();
 
 const showModalProviders = (id) => {
@@ -15,6 +21,28 @@ const showModalProviders = (id) => {
 const showProvider = (data) => {
   data.forEach((item) => renderProvider(providerParent, item));
 };
+
+// show mappeds by country
+const showProviderByCountry = (data, country) => {
+  const result = data.filter((item) => item.pais === country);
+
+  providerParent.innerHTML = "";
+  result.forEach((item) => renderProvider(providerParent, item));
+};
+
+const showProviderBySector = (data, sector) => {
+  const result = data.filter((item) => item.sector === sector);
+
+  providerParent.innerHTML = "";
+  result.forEach((item) => renderProvider(providerParent, item));
+};
+
+const init = () => {
+  showProvider(JSON.parse(res.value));
+};
+init();
+
+// EVENT HANDLERS
 
 providerParent.addEventListener("click", function (e) {
   const childElement = e.target.closest(".modal-button--open");
@@ -28,7 +56,48 @@ providerParent.addEventListener("click", function (e) {
   const modal = new Modal(body, result, 2);
 });
 
-const init = () => {
+buttonParent.addEventListener("click", function (e) {
+  const button = e.target.closest(".button-filter");
+  const id = button.getAttribute("data-tab");
+
+  // Reset providers
   showProvider(JSON.parse(res.value));
-};
-init();
+
+  // Reset active
+  buttonFilter.forEach((button) => {
+    button.classList.remove("button-filter--active");
+  });
+
+  // Reset provider message
+  providerMessage.forEach((message) => {
+    message.classList.add("hidden");
+  });
+
+  // Reset filter provider
+  filterProvider.forEach((provider) => {
+    provider.classList.add("hidden");
+  });
+
+  // Show active button filter
+  if (!button.classList.contains("button-filter--active"))
+    button.classList.add("button-filter--active");
+
+  // Show initiative message
+  providerMessage[id].classList.remove("hidden");
+
+  // Show filter provider
+  if (!filterProvider[id]) return;
+  filterProvider[id].classList.remove("hidden");
+});
+
+// Show provider by countries event
+selectCountries.addEventListener("change", function (e) {
+  if (e.target.value === "Todos") showProvider(JSON.parse(res.value));
+  else showProviderByCountry(JSON.parse(res.value), e.target.value);
+});
+
+// Show provider by sector event
+selectSector.addEventListener("change", function (e) {
+  if (e.target.value === "Todos") showProvider(JSON.parse(res.value));
+  else showProviderBySector(JSON.parse(res.value), e.target.value);
+});
